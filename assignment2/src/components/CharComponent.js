8eb0d0a1227578038b514979dbaf20810d14a0a2
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-05 16:41:20 -0500

    rendered Char Input

--- assignment2/src/components/CharComponent.js
+++ assignment2/src/components/CharComponent.js
@@ -2,6 +2,9 @@ import React from "react";
 
 class CharComponent extends React.Component {
   render() {
+    const { input } = this.props;
+    let charInput = input.split("");
+    console.log(charInput);
     return (
       <div>
         {this.props.input.length > 5 ? "Text Too Long" : this.props.input}
